7b4c90464028522e782f7882da10f36ea75648d9
MarinaraSky <36424779+MarinaraSky@users.noreply.github.com>
2018-03-15 11:46:32 -0400

    Update Forward-Reverse.ino

--- Forward-Reverse.ino
+++ Forward-Reverse.ino
@@ -1,38 +1,28 @@
-const int analogInPin = A0;
-
-
-int potVal = 0;
-int speed = 0;
-
 void setup()
 {
-  pinMode(9, OUTPUT);
-  pinMode(6, OUTPUT);
+  pinMode(10, OUTPUT);
+  pinMode(11, OUTPUT);
   Serial.begin(9600);
 }
 
 void loop()
-{  
-  potVal = analogRead(analogInPin);
-  speed = map(potVal, 0, 1023, 255, -255);
-  
-  Serial.print("Pot Val: ");
-  Serial.print(potVal);
-  if(potVal >= 501 && potVal <= 530){
+{
+  int potench = analogRead(A1);
+  int speed = map(potench, 0, 1023, 255, -255);
+  if(potench >= 501 && potench <= 530){
     speed = 0;
-    analogWrite(9, speed);
-    analogWrite(6, speed);
+    analogWrite(10, speed);
+    analogWrite(11, speed);
   }
-  if(potVal <= 500){
-    analogWrite(9, speed);
-    analogWrite(6, 0);
+  if(potench <= 500){
+    analogWrite(11, speed);
+    analogWrite(10, 0);
   }
   else{
     speed = speed * -1;
-    analogWrite(6, speed);
-    analogWrite(9, 0);
+    analogWrite(10, speed);
+    analogWrite(11, 0);
   }
-  Serial.print("Speed: ");
   Serial.print(speed);
-  delay(1000);
+  Serial.print(" ");
 }
